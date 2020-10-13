844e698458d240b4ebc1ec6f7b7a7d3a6fc9c1d9
IbrahimChennaoui <ibra.anarchy@gmail.com>
2020-10-13 03:05:11 +0200

    new

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,7 +13,6 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 
 // Protect all routes after this middleware
 router.use(authController.protect);
-
 router.patch('/updateMyPassword', authController.updatePassword);
 router.get('/me', userController.getMe, userController.getUser);
 router.patch(
